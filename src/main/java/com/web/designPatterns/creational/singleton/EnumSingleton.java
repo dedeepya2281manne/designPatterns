da13cccb6a2f4b218260a327553dfb4aa68fa1ea
MANNE DEDEEPYA <dedeepyamanne02@gmail.com>
2025-05-14 07:12:23 +0530

    Create EnumSingleton.java

---
+++ src/main/java/com/web/designPatterns/creational/singleton/EnumSingleton.java
@@ -0,0 +1,8 @@
+package com.web.designPatterns.creational.singleton;
+public enum EnumSingleton {
+    INSTANCE;
+
+    public void doSomething() {
+        System.out.println("Singleton with Enum is working!");
+    }
+}
